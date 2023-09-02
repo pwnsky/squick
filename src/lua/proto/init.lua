3b8522cd161d2f8b9ca008c519fc62de551befb1
xulvguo <l418894113@gmail.com>
2023-09-02 16:44:44 +0800

    lua proto init

---
+++ src/lua/proto/init.lua
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- Author: i0gan
+-- Email : l418894113@gmail.com
+-- Date  : 2023-09-01
+-- Description: proto 初始化脚本
+-----------------------------------------------------------------------------
+
+require "proto.enum"
+Squick:ImportProto("base.proto");
+Squick:ImportProto("server.proto");
+Squick:ImportProto("game.proto");
+Squick:ImportProto("game_mgr.proto");
+Squick:ImportProto("db_proxy.proto");
+Squick:ImportProto("world.proto");
+Squick:ImportProto("player_event.proto");
+Squick:ImportProto("player.proto");
+Squick:ImportProto("login.proto");
+Squick:ImportProto("micro.proto");
