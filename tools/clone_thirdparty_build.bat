fef285113d4be96442a983d885a1d8de61d31d03
xulvguo <l418894113@gmail.com>
2023-10-01 22:26:24 +0800

    Update clone_thirdparty_build.bat

--- tools/clone_thirdparty_build.bat
+++ tools/clone_thirdparty_build.bat
@@ -1,4 +1,5 @@
 git clone https://github.com/pwnsky/squick-thirdparty-build.git
 del /f /q /s  ..\third_party\build
+mkdir ..\third_party\build
 xcopy /s /e /y squick-thirdparty-build\Windows\build ..\third_party\build
 del /f /q /s  ..\squick-thirdparty-build
