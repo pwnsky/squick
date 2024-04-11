11a14af5bc4f6c4a777734a2f7a2b69bc842ce02
i0gan <l418894113@gmail.com>
2024-04-11 21:21:48 +0800

    Update:Added database client script

---
+++ script/db_client/mongo.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+docker exec -it squick_db_mongo_1 /usr/bin/mongosh -u admin -p pwnsky_squick
---
+++ script/db_client/mysql.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+docker exec -it squick_db_mysql_1 /bin/mysql -uroot -ppwnsky_squick
