fc679072924d01764086e10ae725ded71c1d26c2
i0gan <l418894113@gmail.com>
2024-04-23 17:22:15 +0800

    Update: Merge all field name to one

--- src/node/db_proxy/clickhouse/clickhouse_module.cc
+++ src/node/db_proxy/clickhouse/clickhouse_module.cc
@@ -176,60 +176,71 @@ void ClickhouseModule::OnReqSelect(const socket_t sock, const int msg_id, const 
     try {
         assert(m_net_->ReceivePB(msg_id, msg, len, req, uid));
         std::vector<Block> all_blocks;
+        Block merged_block;
+        std::map<std::string, rpc::ClickhouseData *> all_field_data;
         client_->Select(req.sql(), [&](const Block& block) {
-            all_blocks.push_back(block);
-        });
-        for (auto& block : all_blocks) {
+            if (block.GetRowCount() <= 0) {
+                return;
+            }
             for (size_t col = 0; col < block.GetColumnCount(); ++col) {
                 auto type_code = block[col]->GetType().GetCode();
-                auto ack_data = ack.add_data();
-                ack_data->set_field(block.GetColumnName(col));
+                std::string field_name = block.GetColumnName(col);
+                auto field_iter = all_field_data.find(field_name);
+                rpc::ClickhouseData *field_data  = nullptr;
+                if (field_iter == all_field_data.end()) {
+                    field_data = ack.add_data();
+                    field_data->set_field(field_name);
+                    all_field_data[field_name] = field_data;
+                } else {
+                    field_data = field_iter->second;
+                }
+                
                 for (size_t row = 0; row < block.GetRowCount(); ++row) {
                     switch (type_code) {
                     case Type::Code::UInt8:
-                        ack_data->set_type(rpc::ClickHouseDataTypeUInt8);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnUInt8>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeUInt8);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnUInt8>()->At(row)));
                         break;
                     case Type::Code::UInt16:
-                        ack_data->set_type(rpc::ClickHouseDataTypeUInt16);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnUInt16>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeUInt16);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnUInt16>()->At(row)));
                         break;
                     case Type::Code::UInt32:
-                        ack_data->set_type(rpc::ClickHouseDataTypeUInt32);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnUInt32>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeUInt32);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnUInt32>()->At(row)));
                         break;
                     case Type::Code::Int8:
-                        ack_data->set_type(rpc::ClickHouseDataTypeInt8);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnInt8>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeInt8);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnInt8>()->At(row)));
                         break;
                     case Type::Code::Int16:
-                        ack_data->set_type(rpc::ClickHouseDataTypeInt16);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnInt16>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeInt16);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnInt16>()->At(row)));
                         break;
                     case Type::Code::Int32:
-                        ack_data->set_type(rpc::ClickHouseDataTypeInt32);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnInt32>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeInt32);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnInt32>()->At(row)));
                         break;
                     case Type::Code::UInt64:
-                        ack_data->set_type(rpc::ClickHouseDataTypeUInt64);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnUInt64>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeUInt64);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnUInt64>()->At(row)));
                         break;
                     case Type::Code::Int64:
-                        ack_data->set_type(rpc::ClickHouseDataTypeInt64);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnInt64>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeInt64);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnInt64>()->At(row)));
                         break;
                     case Type::Code::Float32:
-                        ack_data->set_type(rpc::ClickHouseDataTypeFloat32);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnFloat32>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeFloat32);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnFloat32>()->At(row)));
                         break;
                     case Type::Code::Float64:
-                        ack_data->set_type(rpc::ClickHouseDataTypeFloat64);
-                        ack_data->add_values(std::to_string(block[col]->As<ColumnFloat64>()->At(row)));
+                        field_data->set_type(rpc::ClickHouseDataTypeFloat64);
+                        field_data->add_values(std::to_string(block[col]->As<ColumnFloat64>()->At(row)));
                         break;
                     case Type::Code::String: {
-                        ack_data->set_type(rpc::ClickHouseDataTypeString);
+                        field_data->set_type(rpc::ClickHouseDataTypeString);
                         std::string db_string(block[col]->As<ColumnString>()->At(row));
-                        ack_data->add_values(db_string);
+                        field_data->add_values(db_string);
                     } break;
                     default:
                         LOG_ERROR("Not surpport this clickhouse data type: %v", (int)type_code);
@@ -237,20 +248,16 @@ void ClickhouseModule::OnReqSelect(const socket_t sock, const int msg_id, const 
                     }
                 }
             }
-        }
-
+        });
         ack.set_code(code);
-        ack.set_query_id(req.query_id());
-        m_net_->SendPBToNode(rpc::IdNAckClickhouseSelect, ack, sock);
-        LOG_INFO("Clickhouse select callbacks: sql<%v> col<%v>", req.sql(), ack.data_size());
-
     } catch (std::exception e) {
-        
         LogError(e.what(), __func__, __LINE__);
         ack.set_code(rpc::DB_PROXY_CODE_CLICKHOUSE_EXCEPTION);
-        ack.set_query_id(req.query_id());
-        m_net_->SendPBToNode(rpc::IdNAckClickhouseSelect, ack, sock);
     }
+
+    ack.set_query_id(req.query_id());
+    m_net_->SendPBToNode(rpc::IdNAckClickhouseSelect, ack, sock);
+    LOG_INFO("Clickhouse select callbacks: sql<%v> col<%v>", req.sql(), ack.data_size());
 }
 
 bool ClickhouseModule::Connect() {
