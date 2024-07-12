e9d7b9e7d5abf092e130d8b5331debc305462cc9
i0gan <l418894113@gmail.com>
2024-07-12 21:24:02 +0800

    Update: Speed up compile time when compiling a single plugin

--- tools/build_squick.sh
+++ tools/build_squick.sh
@@ -8,35 +8,41 @@
 cd $(dirname $0)
 source common.sh
 
-# 将Proto文件生成代码
-log_info "Generate protocol c++ files from proto files"
-cd ${project_path}/tools
-time bash ./proto2code.sh
-check_err
-
-# 生成配置文件
-log_info "Generate configure files"
-cd ${project_path}/tools/
-time bash ./generate_config.sh
-check_err
-
-# 初始化第三方.so
-log_info "Init runtime env"
-cd ${project_path}/tools/
-time bash ./init_runtime_dll.sh
-check_err
+function init_env()
+{
+    # 将Proto文件生成代码
+    log_info "Generate protocol c++ files from proto files"
+    cd ${project_path}/tools
+    time bash ./proto2code.sh
+    check_err
+    
+    # 生成配置文件
+    log_info "Generate configure files"
+    cd ${project_path}/tools/
+    time bash ./generate_config.sh
+    check_err
+    
+    # 初始化第三方.so
+    log_info "Init runtime env"
+    cd ${project_path}/tools/
+    time bash ./init_runtime_dll.sh
+    check_err
+}
 
 log_info "Start to build squick"
 build_squick() {
 	cd ${project_path}
 	mkdir -p "${build_path}"
-	cd "${build_path}"
-	cmake ${project_path}  -DBUILD_VERSION=$build_version -DMODE=$build_mode
 	if [ $# -gt 0 ]; then
 		echo "Compile $@"
+	    cd "${build_path}"
+	    cmake ${project_path}  -DBUILD_VERSION=$build_version -DMODE=$build_mode
 		make $@ -j $(nproc)
 	else
 		echo "Compile all"
+        init_env
+	    cd "${build_path}"
+	    cmake ${project_path}  -DBUILD_VERSION=$build_version -DMODE=$build_mode
 		make -j $(nproc)
 	fi
 }
