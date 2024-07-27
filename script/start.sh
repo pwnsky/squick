189e818107ec96e6be8cab7cb1790cfceab85a88
i0gan <l418894113@gmail.com>
2024-07-27 17:55:31 +0800

    Bugfix: Not start the player node

--- script/start.sh
+++ script/start.sh
@@ -16,8 +16,8 @@ sleep 0.5
 sleep 0.5
 ./squick.sh type=web id=2 area=0 ip=127.0.0.1 port=10301 http_port=8088 master_ip=127.0.0.1 master_port=10001 logshow=0 &
 sleep 0.5
-./squick.sh type=lobby id=1000 area=0 ip=127.0.0.1 port=10401 master_ip=127.0.0.1 master_port=10001 logshow=0 &
+./squick.sh type=player id=1000 area=0 ip=127.0.0.1 port=10401 master_ip=127.0.0.1 master_port=10001 logshow=0 &
 sleep 0.5
-./squick.sh type=lobby id=1001 area=0 ip=127.0.0.1 port=10402 master_ip=127.0.0.1 master_port=10001 logshow=0 &
+./squick.sh type=player id=1001 area=0 ip=127.0.0.1 port=10402 master_ip=127.0.0.1 master_port=10001 logshow=0 &
 sleep 0.5
 ./squick.sh type=proxy id=500 area=0 ip=127.0.0.1 port=10501 ws_port=10502 master_ip=127.0.0.1 master_port=10001 logshow=0 &
