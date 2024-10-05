aa662bb44392886aacdb1928f1f2d696a4f996e1
i0gan <l418894113@gmail.com>
2024-10-05 18:01:35 +0800

    Update deploy script

--- tools/gen_deploy.sh
+++ tools/gen_deploy.sh
@@ -5,6 +5,7 @@
 # Github: https://github.com/pwnsky/squick
 # Description: Generate Linux or Macos deploy files
 cd $(dirname $0)
+source common.sh
 echo "Generating linux or macos deploy files"
 bash init_runtime_dll.sh
 
@@ -24,16 +25,19 @@ cp -r res/XlsxXML deploy/res/XlsxXML
 cp -r res/Proto deploy/res/Proto
 
 # copy lib
-cp /lib/x86_64-linux-gnu/libssl.so.1.1    deploy/bin
-cp /lib/x86_64-linux-gnu/libcrypto.so.1.1 deploy/bin
-
-cp /lib/x86_64-linux-gnu/libssl.so    deploy/bin
-cp /lib/x86_64-linux-gnu/libcrypto.so deploy/bin
+echo "Deploy for $LinuxDistro"
+if [ $LinuxDistro == "Debian" ] || [ $LinuxDistro == "Ubuntu" ] || [ $LinuxDistro == "Raspbian" ]; then
+    cp /lib/x86_64-linux-gnu/libssl.so.1.1    deploy/bin
+    cp /lib/x86_64-linux-gnu/libcrypto.so.1.1 deploy/bin
+    cp /lib/x86_64-linux-gnu/libssl.so    deploy/bin
+    cp /lib/x86_64-linux-gnu/libcrypto.so deploy/bin
+elif [ $LinuxDistro == "Arch" ]; then
+    echo "Arch"
+fi
 
 # create dir
 cd deploy
 mkdir -p data/logs
 mkdir -p data/crashs
-
 
 echo "Generated all files!"
